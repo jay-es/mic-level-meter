26d2e961ac192c393fdca0841a3e1b5d61f70526
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2024-06-14 20:28:15 +0900

    Electron 移行

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require("electron/main");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    title: "Mic Level Meter",
+    width: 32,
+    height: 680,
+    alwaysOnTop: true,
+    resizable: false,
+    maximizable: false,
+    frame: false,
+    transparent: true,
+  });
+
+  win.loadFile("./client/index.html");
+
+  if (process.argv.includes("--devtools")) {
+    win.webContents.openDevTools({ mode: "detach" });
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
