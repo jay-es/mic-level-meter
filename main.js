5d0ab3dad28b8028f582a709c1fcc5642aa1c370
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2024-06-15 22:00:05 +0900

    ウィンドウ位置の保存・復元

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const { app, BrowserWindow } = require("electron/main");
+const windowStateKeeper = require("electron-window-state");
 
 function createWindow() {
+  const mainWindowState = windowStateKeeper();
+
   const win = new BrowserWindow({
     title: "Mic Level Meter",
     width: 32,
     height: 142,
+    x: mainWindowState.x,
+    y: mainWindowState.y,
     alwaysOnTop: true,
     resizable: false,
     maximizable: false,
@@ -13,6 +18,11 @@ function createWindow() {
   });
 
   win.loadFile("./client/index.html");
+
+  // ウィンドウを動かしたら位置を記録（manage メソッドだと記録されなかった）
+  win.on("moved", () => {
+    mainWindowState.saveState(win);
+  });
 
   if (process.argv.includes("--devtools")) {
     win.webContents.openDevTools({ mode: "detach" });
